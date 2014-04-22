6d1d53ec5f09341965ca3e3277c211b1f553cf6c
jeliecerv <jorge.valencia@newshore.es>
2014-04-22 15:40:41 -0500

    reLayout en productos

--- website/scripts/products.js
+++ website/scripts/products.js
@@ -1,14 +1,15 @@
-﻿window.onload = function() {
+﻿window.onload = function () {
     var $container = $('#container-products');
     var animateCategories = false;
     var selector = '';
 
     $container.isotope({
-        itemSelector : '.item',
+        itemSelector: '.item',
+        resizesContainer: true
     });
 
     // filter items when filter link is clicked
-    $('#container-categories > div').click(function(){
+    $('#container-categories > div').click(function () {
         $('#container-categories > div > div').removeClass('active_subcategory');
         $(this).find("div:eq(0)").addClass('active_subcategory');
         selector = $(this).attr('data-filter');
@@ -21,32 +22,31 @@
         $container.isotope({ filter: selector });
     }
 
-    $(".item").click(function() {
+    $(".item").click(function () {
         var $target = $(this);
         var is_visible = $target.find("div:eq(1) > p").is(":visible");
         $(".item > div").removeClass('active_subcategory');
 
-        $(".item").each(function() {
+        $(".item").each(function () {
             var $targeteach = $(this);
             $targeteach.find("div:eq(1)").animate({
                 "margin-top": "11px"
-            }, 500, function() {
-                $targeteach.find("div:eq(1) > p").hide("slow");
+            }, 500, function () {
+                $targeteach.find("div:eq(1) > p").hide("slow", function () {
+                    $container.isotope('reLayout');
+                });
+                
             });
         });
-        
-        if(!is_visible) {
+
+        if (!is_visible) {
             var marginTop = ($target.find("div:eq(1)").css("margin-top") == "-3px") ? "11px" : "-3px";
             $target.find("div:eq(0)").addClass('active_subcategory');
             $target.find("div:eq(1)").animate({
                 "margin-top": marginTop
-            }, 500, function() {
-                $target.find("div:eq(1) > p").show("slow", function() {
-                    if($target.parent().height() < $target.height()) {
-                        $target.parent().animate({
-                            "height": $target.height()
-                        });
-                    }
+            }, 500, function () {
+                $target.find("div:eq(1) > p").show("slow", function () {
+                    $container.isotope('reLayout');
                 });
             });
         }
